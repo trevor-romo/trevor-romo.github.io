7f29dbc310332f45bfef5c652f53d261d96112cf
Trevor Romatoski <300102472@fvtc.edu>
2025-02-15 20:53:16 -0600

    Added Secret Message

---
+++ site.js
@@ -0,0 +1,4 @@
+// store the secret message in localStorage
+localStorage.setItem("It's a secret to everybody.",);
+// verifying log
+console.log(localStorage.getItem("It's a secret to everybody."))
